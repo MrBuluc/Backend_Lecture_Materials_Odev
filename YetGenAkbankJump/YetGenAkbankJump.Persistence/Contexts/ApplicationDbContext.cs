d0795741e84c57f1fb95f58f345200b774aa8898
Hakkıcan Bülüç <43816007+MrBuluc@users.noreply.github.com>
2023-11-18 20:07:39 +0300

    completed Persistence

---
+++ YetGenAkbankJump/YetGenAkbankJump.Persistence/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using YetGenAkbankJump.Domain.Entities;
+
+namespace YetGenAkbankJump.Persistence.Contexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Category> Categories {  get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> dbContextOptions) : base(dbContextOptions) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
---
+++ YetGenAkbankJump/YetGenAkbankJump.Persistence/Contexts/ApplicationDbContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace YetGenAkbankJump.Persistence.Contexts
+{
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();
+
+            DbContextOptionsBuilder<ApplicationDbContext> optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+
+            optionsBuilder.UseNpgsql(configuration.GetSection("YetgenPostgreSQLDB").Value);
+
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
