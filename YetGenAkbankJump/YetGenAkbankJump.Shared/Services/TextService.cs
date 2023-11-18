c4afbc968e07b1333fdb8daf308f540e628e9f98
Hakkıcan Bülüç <43816007+MrBuluc@users.noreply.github.com>
2023-11-18 16:00:10 +0300

    updated BlazorClient, Domain, Shared/Services and WebApi

---
+++ YetGenAkbankJump/YetGenAkbankJump.Shared/Services/TextService.cs
@@ -0,0 +1,11 @@
+﻿namespace YetGenAkbankJump.Shared.Services
+{
+    public class TextService
+    {
+
+        public void Save(string path)
+        {
+            File.WriteAllText(_path, text);
+        }
+    }
+}
---
+++ YetGenAkbankJump/YetGenAkbankJump.WebApi/Controllers/CarsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using YetGenAkbankJump.Domain.Entities;
+using YetGenAkbankJump.WebApi.Data;
+
+namespace YetGenAkbankJump.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(CarsContext.LuxuryCars.ToList());
+        }
+
+        [HttpGet("{id:guid}")]
+        public IActionResult GetById(Guid id)
+        {
+            if (id == Guid.Empty)
+            {
+                return BadRequest("Id cannot be empty.");
+            }
+
+            Car car = CarsContext.LuxuryCars.FirstOrDefault(x => x.Id == id);
+            if (car == null)
+            {
+                return NotFound("The car requested with given id was not found. ");
+            }
+            return Ok(car);
+        }
+    }
+}
